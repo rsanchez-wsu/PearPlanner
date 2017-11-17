6295f52b520127ded8e6d0dff42a0918af4c2128
Fahad-n <alnashwan.2@wright.edu>
2017-11-17 14:55:47 -0500

    fixing CheckStyle in ModelEntity.java part of issue fixes #9

--- src/Model/ModelEntity.java
+++ src/Model/ModelEntity.java
@@ -27,98 +27,78 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 /**
- * PearPlanner/RaiderPlanner
- * Created by Team BRONZE on 4/27/17
+ * PearPlanner/RaiderPlanner Created by Team BRONZE on 4/27/17
  */
-public class ModelEntity implements Serializable
-{
-    protected String name = "";
-    protected MultilineString details = null;
-    protected ArrayList<Note> notes;
+public class ModelEntity implements Serializable {
+	protected String name = "";
+	protected MultilineString details = null;
+	protected ArrayList<Note> notes;
 
+	// getters
+	public String getName() {
+		return name;
+	}
 
-    // getters
-    public String getName()
-    {
-        return name;
-    }
+	public MultilineString getDetails() {
+		return details;
+	}
 
-    public MultilineString getDetails()
-    {
-        return details;
-    }
+	public void setName(String newName) {
+		name = newName;
+	}
 
-    public void setName(String newName)
-    {
-        name = newName;
-    }
+	public void setDetails(String newDetails) {
+		details = new MultilineString(newDetails);
+	}
 
-    public void setDetails(String newDetails)
-    {
-        details = new MultilineString(newDetails);
-    }
+	public void setDetails(String[] newDetails) {
+		details = new MultilineString(newDetails);
+	}
 
-    public void setDetails(String[] newDetails)
-    {
-        details = new MultilineString(newDetails);
-    }
+	public void setDetails(ArrayList<String> newDetails) {
+		details = new MultilineString(newDetails.toArray(new String[newDetails.size()]));
+	}
 
-    public void setDetails(ArrayList<String> newDetails)
-    {
-        details = new MultilineString(newDetails.toArray(new String[newDetails.size()]));
-    }
+	public void setDetails(MultilineString newDetails) {
+		details = newDetails;
+	}
 
-    public void setDetails(MultilineString newDetails)
-    {
-        details = newDetails;
-    }
+	public void addProperties(String aName, MultilineString aDetails) {
+		setName(aName);
+		setDetails(aDetails.clone());
+	}
 
+	public void addProperties(String aName, String aDetails) {
+		setName(aName);
+		setDetails(aDetails);
+	}
 
-    public void addProperties(String aName, MultilineString aDetails)
-    {
-        setName(aName);
-        setDetails(aDetails.clone());
-    }
+	/**
+	 * Open the appropriate UI window for this class To be overridden by children.
+	 */
+	public void open(MenuController.Window current) {
+	}
 
-    public void addProperties(String aName, String aDetails)
-    {
-        setName(aName);
-        setDetails(aDetails);
-    }
+	public ModelEntity() {
+		this("");
+	}
 
-    /**
-     * Open the appropriate UI window for this class
-     * To be overridden by children.
-     */
-    public void open(MenuController.Window current)
-    {
-    }
+	public ModelEntity(String cName) {
+		this(cName, "");
+	}
 
-    public ModelEntity()
-    {
-        this("");
-    }
+	public ModelEntity(String cName, String cDetails) {
+		this(cName, cDetails.split("\n"));
+	}
 
-    public ModelEntity(String cName)
-    {
-        this(cName, "");
-    }
+	public ModelEntity(String cName, String[] cDetails) {
+		setName(cName);
+		setDetails(cDetails);
+		notes = new ArrayList<>();
+	}
 
-    public ModelEntity(String cName, String cDetails)
-    {
-        this(cName, cDetails.split("\n"));
-    }
-
-    public ModelEntity(String cName, String[] cDetails)
-    {
-        setName(cName);
-        setDetails(cDetails);
-        notes = new ArrayList<>();
-    }
-
-    public ModelEntity(String cName, String[] cDetails, ArrayList<Note> cNotes)
-    {
-        this(cName, cDetails);
-        notes = (ArrayList<Note>) cNotes.clone();
-    }
+	public ModelEntity(String cName, String[] cDetails, ArrayList<Note> cNotes) {
+		this(cName, cDetails);
+		notes = (ArrayList<Note>) cNotes.clone();
+	}
 }
