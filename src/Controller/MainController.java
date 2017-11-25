2d04ec176005843a8ca6e25f3d5efcbedbad58e0
NguyenVo <vo.8@wright.edu>
2017-11-25 01:34:59 -0500

    Implement test case for MainControllerTest
    refer issue #177

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -93,11 +93,14 @@ public class MainController {
 	 * Initializes the Study Planner by either registering a new account or
 	 * importing an existing Study Planner file.
 	 */
-	public static void initialise() {
+	//changed return type for testing purposes
+	public static boolean initialise() {
+		boolean rtn = false;
 		try {
 			ui.showStartup();
 		} catch (IOException e) {
 			UIManager.reportError("Invalid file.");
+			rtn = false;
 			System.exit(1);
 		}
 		// If a file is present:
@@ -119,6 +122,7 @@ public class MainController {
 								+ "restart the application "
 								+ "and choose \"New File\".");
 					}
+					rtn = true;
 				}
 			} catch (FileNotFoundException e) {
 				UIManager.reportError("Error, File does not exist.");
@@ -154,8 +158,10 @@ public class MainController {
 			// or existence in the milliseconds that it runs the above code
 			// after checks in StartupController
 			UIManager.reportError("Failed to load file.");
+			rtn = false;
 			System.exit(1);
 		}
+		return rtn;
 	}
 
 	/**
