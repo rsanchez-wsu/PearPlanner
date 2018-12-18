839dc50e5b6bba6c6d82fd1e59380db5efe982bf
RichardSimmons <simmons.105@wright.edu>
2018-12-17 21:14:44 -0500

    Issue 422 Dialog box error
    This commit fixes a logistical error in the salutation message that prevented a dialog message to appear when no name was entered.

--- src/edu/wright/cs/raiderplanner/controller/AccountController.java
+++ src/edu/wright/cs/raiderplanner/controller/AccountController.java
@@ -170,6 +170,7 @@ public class AccountController implements Initializable {
 		String invalidMessage = "";
 		boolean validSuccess = true;
 		boolean validName = true;
+		
 		if (!validateNumber()) {
 			invalidMessage += "Please enter a valid W Number\n";
 			validSuccess = false;
@@ -182,10 +183,7 @@ public class AccountController implements Initializable {
 			invalidMessage += "Please enter a valid email\n";
 			validSuccess = false;
 		}
-		if (!validateSalutation()) {
-			invalidMessage += "Please enter a valid salutation\n";
-			validSuccess = false;
-		}
+		
 		if (this.fullName.getText().trim().isEmpty()) {
 			if (!this.handleEmptyName()) {
 				validName = false;
